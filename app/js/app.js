23e18bfecd7805520227cc1a5930725aed57f658
Mike <myjubox@gmail.com>
2015-01-04 00:06:48 +0200

    Adding basic login form and show all ads page.

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,16 @@
 var selloBuyer = angular.module('selloBuyerModule', ['ngRoute'])
-    .config(function($routeProvider) {
-$routeProvider.when('/register', {
-    templateUrl:'templates/registration.html',
-    controller: 'loginController'
-})
+    .config(function ($routeProvider) {
+        $routeProvider.when('/register', {
+            templateUrl: 'templates/registration.html',
+            controller: 'registrationController'
+        });
+        $routeProvider.when('/login', {
+            templateUrl: 'templates/login.html',
+            controller: 'loginController'
+        });
+        $routeProvider.when('/ads', {
+            templateUrl: 'templates/ads.html',
+            controller: 'loginController'
+        })
+        $routeProvider.otherwise({redirectTo: '/ads'})
     });
