a1dcf8b819418308ffa654bafdccb6d25b51bcf3
Mike <myjubox@gmail.com>
2015-01-04 17:11:35 +0200

    Adding town and category menu.

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -2,5 +2,15 @@ selloBuyer.controller('SelloBuyerController', function($scope, loadAds) {
     loadAds.getAllAds(function(resp)
     {
         $scope.data = resp
+    });
+
+    loadAds.getAllTowns(function(resp)
+    {
+        $scope.towns = resp
+    });
+
+    loadAds.getAllCategories(function(resp)
+    {
+        $scope.categories = resp
     })
 });
--- app/js/services/loadAdsService.js
+++ app/js/services/loadAdsService.js
@@ -8,6 +8,26 @@ selloBuyer.factory('loadAds', function ($http, $log) {
                 .error(function (data, sstatus, headers, config) {
                     $log.warn(data)
                 })
+        },
+
+        getAllTowns: function (success) {
+            $http({method: 'GET', url: 'http://localhost:1337/api/towns'})
+                .success(function (data, status, headers, config) {
+                    success(data)
+                })
+                .error(function (data, sstatus, headers, config) {
+                    $log.warn(data)
+                })
+        },
+
+        getAllCategories: function (success) {
+            $http({method: 'GET', url: 'http://localhost:1337/api/categories'})
+                .success(function (data, status, headers, config) {
+                    success(data)
+                })
+                .error(function (data, sstatus, headers, config) {
+                    $log.warn(data)
+                })
         }
     }
 });
