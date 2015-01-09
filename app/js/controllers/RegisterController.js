835a425e6d95991dd5ea8089bdf46fa77c066475
Mike <myjubox@gmail.com>
2015-01-09 15:23:45 +0200

    Register

--- app/js/controllers/RegisterController.js
+++ app/js/controllers/RegisterController.js
@@ -1,5 +1,20 @@
 'use strict';
 
 app.controller('RegisterController', function ($scope, $location, townsService, authService, notifyService) {
+        $scope.userData = {townId: null};
 
-});
+        $scope.towns = townsService.getTowns();
+
+        $scope.register = function(userData) {
+            authService.register(userData,
+                function success() {
+                    notifyService.showInfo("Registration successful");
+                    $location.path("/");
+                },
+                function error(err) {
+                    notifyService.showError("User registration failed", err);
+                }
+            );
+        };
+    }
+);
