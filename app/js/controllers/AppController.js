853052c0334258655e438997c714c258302f2bb9
Mike <myjubox@gmail.com>
2015-01-09 14:38:17 +0200

    Login.

--- app/js/controllers/AppController.js
+++ app/js/controllers/AppController.js
@@ -1,5 +1,13 @@
 'use strict';
 
-app.controller('AppController', function ($scope, authService) {
-    $scope.authService = authService
-});
+app.controller('AppController',
+    function ($scope, $location, authService, notifyService) {
+        $scope.authService = authService;
+
+        $scope.logout = function() {
+            authService.logout();
+            notifyService.showInfo("You have logged out.");
+            $location.path("/");
+        };
+    }
+);
--- app/js/controllers/loginController.js
+++ app/js/controllers/loginController.js
@@ -1,5 +1,15 @@
 'use strict';
 
 app.controller('LoginController', function ($scope, $location, authService, notifyService) {
-
+    $scope.login = function (userData) {
+        authService.login(userData,
+            function success() {
+                notifyService.showInfo('Successful login.')
+                $location.path("/");
+            },
+            function error(err) {
+                notifyService.showError('Error', err)
+            }
+        )
+    }
 });
